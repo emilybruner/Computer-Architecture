d9654e7a5a80d0d994c5113650a61e92acab06ef
Emily Bruner <bruner.emilym@gmail.com>
2020-05-19 18:46:38 -0400

    day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,6 +5,7 @@ import sys
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
+MUL = 0b10100010
 
 class CPU:
     """Main CPU class."""
@@ -27,24 +28,38 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
+        # counter
         address = 0
+
+        # open ls8 file
+        with open(sys.argv[1]) as f:
+            # read each line
+            for line in f:
+                #split on # to remove comments and empty spaces
+                line_value = line.split("#")[0].strip()
+                if line_value == '':
+                    continue
+                # convert to int (base 2 binary) and save to ram at address
+                value = int(line_value, 2)
+                self.ram[address] = value
+                #increment address counter
+                address += 1
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+            # self.ram[address] = instruction
+            # address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -53,6 +68,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -97,6 +114,10 @@ class CPU:
                 prn_reg = self.ram[self.pc + 1]
                 print(self.reg[prn_reg])
                 self.pc += 2
+
+            elif ir == MUL:
+                self.alu(ir, operand_a, operand_b)
+                self.pc += 3
 
             elif ir == HLT:
                 running = False
